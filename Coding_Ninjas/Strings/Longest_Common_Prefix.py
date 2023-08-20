bedb8bef65edb48469bba4071c8c624a20738606
S GAYATHRI IYER <81139359+SGayathri05@users.noreply.github.com>
2023-08-20 20:21:32 +0530

    Update Longest_Common_Prefix.py

--- Coding_Ninjas/Strings/Longest_Common_Prefix.py
+++ Coding_Ninjas/Strings/Longest_Common_Prefix.py
@@ -4,17 +4,10 @@ def commonPrefix(arr: List[str], n: int) -> str:
     # Write your code here
     res = arr[0]
     n = len(res)
- 
-   
-    for i in range(1, len(arr)):
-       
-        while arr[i].find(res) != 0:
-            
+    for i in range(1, len(arr)):       
+        while arr[i].find(res) != 0:            
             res = res[:n- 1]
             n -= 1
- 
-           
             if not res:
                 return "-1"
- 
     return res
