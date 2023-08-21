5000aece4f481b59e1fd5e449038cee8a2c1d2f2
S GAYATHRI IYER <81139359+SGayathri05@users.noreply.github.com>
2023-08-21 22:25:40 +0530

    Create Contains_Duplicate.py

---
+++ Leetcode/Contains_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        ans = set()
+        for n in nums:
+            if n in ans:
+                return True
+            ans.add(n)
+        return False
